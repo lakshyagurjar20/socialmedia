31c612ce7bae1156b75ada2e2aa8162619983e30
lakshyagurjar20 <lakshygurjar2095@gmail.com>
2025-06-20 21:07:16 +0530

    Add signup, login, logout views and added sign up form

--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
+from django.contrib import messages
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
+    return render(request, 'loginn.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
 
 # Create your views here.
