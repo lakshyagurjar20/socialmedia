3422ed59f4963a018a658a39538351b45820ec37
lakshyagurjar20 <lakshygurjar2095@gmail.com>
2025-06-21 13:27:07 +0530

    Resolved issue of image not uploading of post

--- userauth/models.py
+++ userauth/models.py
@@ -15,7 +15,7 @@ from datetime import datetime
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='post_images')
+    image = models.ImageField(upload_to='post_images/')
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
 
