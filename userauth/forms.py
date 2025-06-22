b2efdd09e1a688ddbdf88d871fdb1f691c236853
lakshyagurjar20 <lakshygurjar2095@gmail.com>
2025-06-22 12:33:25 +0530

    Enhanced signup/login page

--- userauth/forms.py
+++ userauth/forms.py
@@ -3,11 +3,14 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.help_text = ''
 
 from .models import Post
 
